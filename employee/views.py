9d1a00ce234df4ba7a7babaf4a6fd5720ee5e21e
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-03-16 07:51:19 +0000

    format lines

--- employee/views.py
+++ employee/views.py
@@ -18,87 +18,91 @@ import json
 
 
 def home(request):
-    template = 'employee_list.html'
+    template = "employee_list.html"
     # employees = Employee.objects.all()
     items = []
 
     for employee in Employee.objects.iterator(500):
         item = employee.__dict__
-        item['age'] = employee.age
-        item['gender'] = employee.get_gender_display()
-        item['marital_status'] = employee.get_marital_status_display()
-        item['nationality'] = employee.nationality.name
-        item['employment_type'] = employee.get_employment_type_display()
-        del item['nationality_id']
-        del item['_state']
+        item["age"] = employee.age
+        item["gender"] = employee.get_gender_display()
+        item["marital_status"] = employee.get_marital_status_display()
+        item["nationality"] = employee.nationality.name
+        item["employment_type"] = employee.get_employment_type_display()
+        del item["nationality_id"]
+        del item["_state"]
         items.append(item)
 
     json.dumps(items, cls=DjangoJSONEncoder)
 
     context = {
-        'employees': items,
+        "employees": items,
     }
 
-    return render(request,template, context)
+    return render(request, template, context)
 
 
 def employee_get(request, id):
-    template = 'employee_detail.html'
+    template = "employee_detail.html"
     employee = get_object_or_404(Employee, pk=id)
 
     context = {
-        'employee': employee,
+        "employee": employee,
     }
-    return render(request,template, context)
+    return render(request, template, context)
 
 
 def employee_create(request):
 
-    template = 'employee_create.html'
+    template = "employee_create.html"
     context = {}
 
-    AddressFormSet = modelformset_factory(
-        Address, exclude=['id', 'employee'], extra = 1
-    )
+    AddressFormSet = modelformset_factory(Address, exclude=["id", "employee"], extra=1)
 
     employee_form = EmployeeForm(instance=Employee())
     address_formset = AddressFormSet(queryset=Address.objects.none())
     employee_position_form = EmployeePositionForm(instance=EmployeePosition())
 
-    context['employee_form']   = employee_form
-    context['address_formset'] = address_formset
-    context['employee_position_form'] = employee_position_form
+    context["employee_form"] = employee_form
+    context["address_formset"] = address_formset
+    context["employee_position_form"] = employee_position_form
 
     if request.method == "POST":
-        employee_form     = EmployeeForm(request.POST, instance=Employee())
-        address_formset   = AddressFormSet(request.POST)
-        employee_position_form = EmployeePositionForm(request.POST, instance=EmployeePosition())
+        employee_form = EmployeeForm(request.POST, instance=Employee())
+        address_formset = AddressFormSet(request.POST)
+        employee_position_form = EmployeePositionForm(
+            request.POST, instance=EmployeePosition()
+        )
 
-        if employee_form.is_valid() and\
-           employee_position_form.is_valid() and\
-           address_formset.is_valid():
+        if (
+            employee_form.is_valid()
+            and employee_position_form.is_valid()
+            and address_formset.is_valid()
+        ):
             new_employee = employee_form.save()
             new_position = employee_position_form.save(commit=False)
-            new_addresses  = address_formset.save(commit=False)
+            new_addresses = address_formset.save(commit=False)
             for new_address in new_addresses:
-                new_address.employee  = new_employee
+                new_address.employee = new_employee
                 new_address.save()
             new_position.employee = new_employee
             new_position.save()
-            return HttpResponseRedirect('/employee')
+            return HttpResponseRedirect("/employee")
 
         else:
-            context['employee_form']   = employee_form
-            context['address_formset'] = address_formset
-            context['employee_position_form'] = employee_position_form
+            context["employee_form"] = employee_form
+            context["address_formset"] = address_formset
+            context["employee_position_form"] = employee_position_form
     return render(request, template, context)
 
 
 def employee_edit(request, id):
-    template = 'employee_update.html'
+    template = "employee_update.html"
     context = {}
 
-    AddressFormSet = inlineformset_factory(Employee, Address, extra=1, exclude=['id', 'employee'])
+    AddressFormSet = inlineformset_factory(
+        Employee, Address, extra=1, exclude=["id", "employee"]
+    )
 
     employee = get_object_or_404(Employee, pk=id)
     employee_current_position = employee.employeeposition_set.last()
@@ -106,17 +110,21 @@ def employee_edit(request, id):
 
     employee_form = EmployeeForm(request.POST or None, instance=employee)
     address_formset = AddressFormSet(request.POST or None, instance=employee)
-    employee_position_form = EmployeePositionForm(request.POST or None, instance=employee_current_position)
+    employee_position_form = EmployeePositionForm(
+        request.POST or None, instance=employee_current_position
+    )
 
-    context['employee_form']   = employee_form
-    context['address_formset'] = address_formset
-    context['employee_position_form'] = employee_position_form
-    context['employee_id'] = employee.id
+    context["employee_form"] = employee_form
+    context["address_formset"] = address_formset
+    context["employee_position_form"] = employee_position_form
+    context["employee_id"] = employee.id
 
     if request.method == "POST":
-        if employee_form.is_valid() and\
-           employee_position_form.is_valid() and\
-           address_formset.is_valid():
+        if (
+            employee_form.is_valid()
+            and employee_position_form.is_valid()
+            and address_formset.is_valid()
+        ):
             update_employee = employee_form.save()
             update_employee_position = employee_position_form.save(commit=False)
             update_employee_position.employee = update_employee
@@ -124,15 +132,14 @@ def employee_edit(request, id):
             update_employee_position.save()
             address_formset.save()
         else:
-            context['employee_form']   = employee_form
-            context['address_formset'] = address_formset
-            context['employee_position_form'] = employee_position_form
+            context["employee_form"] = employee_form
+            context["address_formset"] = address_formset
+            context["employee_position_form"] = employee_position_form
 
     else:
-        context['employee_form']   = employee_form
-        context['address_formset'] = AddressFormSet(instance=employee)
-        context['employee_position_form'] = employee_position_form
-
+        context["employee_form"] = employee_form
+        context["address_formset"] = AddressFormSet(instance=employee)
+        context["employee_position_form"] = employee_position_form
 
     return render(request, template, context)
 
@@ -140,11 +147,11 @@ def employee_edit(request, id):
 def employee_search(request):
     data = {}
     id = 1
-    if request.method == 'GET':
+    if request.method == "GET":
         employees = Employee.objects.filter(pk=id)
         data = serializers.serialize("json", employees, fields=("id", "name"))
 
     for query in request.GET:
-       print(query, request.GET[query])
+        print(query, request.GET[query])
 
     return JsonResponse(data, safe=False)
