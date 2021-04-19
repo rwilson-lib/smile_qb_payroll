e4e5160b016eda630e4ca6d2909abffdabd2cc90
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-04-19 08:47:21 +0000

    rewrote js codes

--- static/js/script.js
+++ static/js/script.js
@@ -1,64 +1,79 @@
 'use strict';
 
-let addButton = document.querySelector("#add-address-line");
-let totalForms = document.querySelector("#id_form-TOTAL_FORMS");
-let addressFormset = document.querySelectorAll("#address p");
-let container = document.querySelector("#address");
-let countrySelects = document.querySelectorAll("#address select");
-let selectWithChangeHandler = []
+let createAddressBtn = $("#add-address-line");
+let createEarningBtn = $("#add-earning-line");
+let employeeIDGeneratorBtn = $("#employee-id-gen");
 
-let formNum = 1 ? totalForms == null : totalForms.value - 1;
+let dateInputs = $("input[name*=date")
 
-addButton.addEventListener('click', addForm);
+createAddressBtn.click((event) => {
+    event.preventDefault();
+    let addressManagementFormSet = $("#address #id_form-TOTAL_FORMS");
+    let formNum = parseInt(addressManagementFormSet.val());
+    let addressRef = $(`#id_address-wrapper-${formNum - 1}`).clone();
+    let formRegex = RegExp(`form-(\\d){1}-`, 'g'); //Regex to find all instances of the form number    
+    addressRef[0].innerHTML = addressRef[0].innerHTML.replace(formRegex, `form-${formNum}-`);
+    addressRef.attr("id", `id_address-wrapper-${formNum}`);
+    $(addressRef).insertBefore(createAddressBtn);
+    addressManagementFormSet.val(formNum + 1);
+});
 
-function processSelection() {
-    countrySelects = document.querySelectorAll("#address select")
-    countrySelects.forEach(
-        function (node) {
-            if (node.name.endsWith('country')) {
-                if (!selectWithChangeHandler.includes(node)) {
-                    selectWithChangeHandler.push(node)
-                    node.addEventListener('change', function handleSelect() {
-                        state_url = $('#Url').attr('data-url').replace(0, node.value)
-                        node_name = node.name.split('-');
-                        state_id = 'id_' + node_name[0] + '-' + node_name[1] + '-state';
-                        let state = $("#" + state_id);
-                        state.empty()
-                        $.ajax({
-                            method: 'GET',
-                            url: state_url,
-                            dataType: 'json'
-                        }).done(function (data) {
-                            data = $.parseJSON(data);
-                            data.forEach(function (d) {
-                                state.append(`<option value="${d['pk']}">
-                                       ${d['fields']['name']}
-                                  </option>`);
-                            });
-                        });
-                    });
-                }
-            }
-        }
-    )
-}
+createEarningBtn.click((event) => {
+    event.preventDefault();
+    let earningManagementFormSet = $("#employee-earning #id_form-TOTAL_FORMS");
+    let formNum = parseInt(earningManagementFormSet.val());
+    let earningRef = $(`#id_earning-wrapper-${formNum - 1}`).clone();
+    let formRegex = RegExp(`form-(\\d){1}-`, 'g');//Regex to find all instances of the form number    
+    earningRef[0].innerHTML = earningRef[0].innerHTML.replace(formRegex, `form-${formNum}-`);
+    let lineNum = earningRef.find("span.earning-line-num");
+    lineNum.text(parseInt(lineNum.text()) + 1);
+    earningRef.attr("id", `id_earning-wrapper-${formNum}`);
+    earningManagementFormSet.val(formNum + 1);
+    $(earningRef).insertBefore(createEarningBtn);
+});
 
-processSelection()
+employeeIDGeneratorBtn.click((event) => {
+    event.preventDefault();
+    let input = $("input[name='employee_id_number']")
+    input.val("20")
+});
 
-function addForm(e) {
 
-    e.preventDefault();
+dateInputs.attr('autocomplete', 'off');
+dateInputs.datepicker({
+    dateFormat: "yy-mm-dd"
+})
 
-    let formRegex = RegExp(`form-(\\d){1}-`, 'g') //Regex to find all instances of the form number
 
-    formNum++
-    for (let i = 0; i < addressFormset.length; i++) {
-        let newNode = addressFormset[i].cloneNode(true) //Clone the bird form
-        newNode.innerHTML = newNode.innerHTML.replace(formRegex, `form-${formNum}-`)
-        container.insertBefore(newNode, addButton)
-    }
+//function processSelection() {
+    //countrySelects = document.querySelectorAll("#address select")
+    //countrySelects.forEach(
+        //function (node) {
+            //if (node.name.endsWith('country')) {
+                //if (!selectWithChangeHandler.includes(node)) {
+                    //selectWithChangeHandler.push(node)
+                    //node.addEventListener('change', function handleSelect() {
+                        //state_url = $('#Url').attr('data-url').replace(0, node.value)
+                        //node_name = node.name.split('-');
+                        //state_id = 'id_' + node_name[0] + '-' + node_name[1] + '-state';
+                        //let state = $("#" + state_id);
+                        //state.empty()
+                        //$.ajax({
+                            //method: 'GET',
+                            //url: state_url,
+                            //dataType: 'json'
+                        //}).done(function (data) {
+                            //data = $.parseJSON(data);
+                            //data.forEach(function (d) {
+                                //state.append(`<option value="${d['pk']}">
+                                       //${d['fields']['name']}
+                                  //</option>`);
+                            //});
+                        //});
+                    //});
+                //}
+            //}
+        //}
+    //)
+//}
 
-    if (totalForms != null)
-        totalForms.setAttribute('value', `${formNum + 1}`)
-    processSelection()
-}
